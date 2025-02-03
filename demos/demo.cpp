d0d08b2daf0241f455245c676cd3a4a8c77e5cf5
nathandking <nathandk16@gmail.com>
2025-02-03 15:06:36 -0330

    Update

--- demos/demo.cpp
+++ demos/demo.cpp
@@ -221,7 +221,7 @@ void performGeoPath(const std::vector<Vector<3>>& queryPoints,
     // extract closest points
     closestPoints.clear();
     for (const GPUBoundingSphere& b: outputBoundingSpheres) {
-        closestPoints.emplace_back(Vector<3>(b.c[0], b.c[1], b.c[2]));
+        closestPoints.emplace_back(Vector<3>(b.c.x, b.c.y, b.c.z));
     }
 }
 
