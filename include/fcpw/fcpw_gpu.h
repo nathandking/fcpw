de44ed5d8c66b0ab17074b5b3e0517c33b967112
nathandking <nathandk16@gmail.com>
2025-02-03 15:00:04 -0330

    Update

--- include/fcpw/fcpw_gpu.h
+++ include/fcpw/fcpw_gpu.h
@@ -62,7 +62,7 @@ public:
                            bool recordNormals=false);
 
     void computeGeoPath(std::vector<GPUBoundingSphere>& inputBoundingSpheres,
-                                            std::vector<GPUBoundingSphere>& outputBoundingSpheres);`
+                                            std::vector<GPUBoundingSphere>& outputBoundingSpheres);
 
     // finds the closest points on the visibility silhouette in the scene to the given query points.
     // The max radius specifies a conservative radius guess around the query point inside which the
