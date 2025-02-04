ce9b4c81ccd7324624385b07a072ca0ae38e48b8
nathandking <nathandk16@gmail.com>
2025-02-04 11:52:08 -0330

    Update

--- demos/demo.cpp
+++ demos/demo.cpp
@@ -195,15 +195,7 @@ void performClosestPointQueries(const std::vector<Vector<3>>& queryPoints,
     }
 }
 
-template <typename T>
-void performGeoPath(const std::vector<Vector<3>>& queryPoints,
-                                std::vector<Vector<3>>& closestPoints,
-                                T& scene)
-{
-    // do nothing
-}
 
-template <>
 void performGeoPath(const std::vector<Vector<3>>& queryPoints,
                                 std::vector<Vector<3>>& closestPoints,
                                 GPUScene<3>& gpuScene)
