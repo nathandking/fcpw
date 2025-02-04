2c80bef4814c49a2dfa3654a6a019cbc4407e132
nathandking <nathandk16@gmail.com>
2025-02-04 13:30:30 -0330

    Update

--- demos/demo.cpp
+++ demos/demo.cpp
@@ -212,6 +212,11 @@ void performGeoPath(const std::vector<Vector<3>>& queryPoints,
     }
     outputBoundingSpheres = inputBoundingSpheres;
 
+    for(size_t i = 0; i < path.size(); ++i)
+    {
+        std::cout << inputBoundingSpheres.c[i][0] << " " << inputBoundingSpheres.c[i][1] << " " << inputBoundingSpheres[i].c[2] << std:: endl;
+    }
+
     gpuScene.computeGeoPath(inputBoundingSpheres, outputBoundingSpheres);
 
     // extract closest points
